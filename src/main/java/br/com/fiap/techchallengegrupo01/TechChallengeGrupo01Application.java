9ebcbd86112aaf3a07694c3fb572e158046d81bc
SavioRony <savio_ls@hotmail.com>
2023-05-20 11:42:18 -0300

    Criado o projeto

---
+++ src/main/java/br/com/fiap/techchallengegrupo01/TechChallengeGrupo01Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallengegrupo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeGrupo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechChallengeGrupo01Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/techchallengegrupo01/TechChallengeGrupo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techchallengegrupo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechChallengeGrupo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
